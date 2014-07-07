8989a6279217424e05334c13e0b3862dbce586f4
hrabizadeh <hrabizadeh@gmail.com>
2014-07-07 16:50:34 +0430

    debug sign out

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -9,9 +9,6 @@ $(function(){
         var msgS=$('.msg-success');
         var box = $('#loginbox');
         var logout= $(".logout");
-        var loginform=$('#loginform');
-        var loginbtn = $('#btn-login');
-        var loginbox_title = $('#loginbox_title');
         var re =/[^@]+/
         user_name= re.exec(username.val());
 
@@ -20,20 +17,19 @@ $(function(){
             msgW.show();
             msgS.hide();
             logout.hide();
-            loginform.show();
-            loginbtn.show();
         }
         else {
             msgS.text('Welcome ' + user_name + '!');
             msgS.show();
             logout.show().click(function()
             {
-                window.location.reload();
+               $(this).hide();
+               box.show();
+               msgS.hide();
+               msgW.hide();
             });
             msgW.hide();
             box.hide();
-            loginform.hide();
-            loginbtn.hide();
         }
     });
 });
